e16a0698a06172b0f8c4162cc198d4c07305a87e
Delon00 <djplebob32@gmail.com>
2024-05-18 22:09:11 +0000

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+/* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import Navbar from "@/components/navbar/navbar";
