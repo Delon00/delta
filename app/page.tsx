b77624d88108ca1c6bbff725d016b438177a1963
Delon00 <djplebob32@gmail.com>
2024-05-19 13:41:30 +0000

    modified:   app/globals.css 	modified:   app/page.tsx 	modified:   components/navbar/navbarCss.css

--- app/page.tsx
+++ app/page.tsx
@@ -22,13 +22,18 @@ export default function Home() {
     <main>
       <Navbar/>
       <section className="main-hero">
-        <h1 className="hero-title">Architec<span className="color1">ture</span> </h1>
-        <div className="hero-colone1"><div className="blank-hero"></div><Image src={hero1} className="heroimg1" alt="del&co logo"/>
+        <div className="hero-title">
+          <h1>Architecture</h1>
+
+        </div>
+          <div className="hero-colone1"><div className="blank-hero"></div><Image src={hero1} className="heroimg1" alt="del&co logo"/>
           <div className="hero-col-text">
             <h3>Une bonne architecture équilibre la fonctionnalité et l'esthétique. Un bâtiment doit être non seulement beau mais aussi pratique et confortable pour ses utilisateurs.</h3>
           </div>
         </div>
-        <div className="hero-colone2"><h1>Chez Del&Co, nous proposons des maquettes architecturales innovantes et de haute qualité. Nos maquettes sont conçues pour capturer l'essence de vos projets et donner vie à vos idées de manière tangible et visuellement saisissante. Que vous soyez un architecte, un promoteur immobilier ou un particulier, nos maquettes sont conçues pour répondre à vos besoin.</h1></div>
+        <div className="hero-colone2">
+          <h2>Chez Del&Co, nous proposons des maquettes architecturales innovantes et de haute qualité. Nos maquettes sont conçues pour capturer l'essence de vos projets et donner vie à vos idées de manière tangible et visuellement saisissante. Que vous soyez un architecte, un promoteur immobilier ou un particulier, nos maquettes sont conçues pour répondre à vos besoin.</h2>
+        </div>
         <div className="hero-colone3"><Image src={hero2} className="heroimg" alt="del&co logo"/></div>
       </section>
 
