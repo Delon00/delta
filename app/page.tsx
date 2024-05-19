0c14d70b31bd777688ac9f6801e0ae4ac8401d45
Delon00 <djplebob32@gmail.com>
2024-05-19 10:51:57 +0000

    modified:   .eslintrc.json 	modified:   app/favicon.ico 	modified:   app/globals.css 	modified:   app/layout.tsx 	modified:   app/page.tsx 	modified:   components/navbar/navbar.jsx 	modified:   components/navbar/navbarCss.css 	modified:   package-lock.json 	modified:   package.json 	new file:   public/assets/android-chrome-192x192.png 	new file:   public/assets/android-chrome-512x512.png 	new file:   public/assets/apple-touch-icon.png 	new file:   public/assets/close.png 	new file:   public/assets/del&co favicon.png 	new file:   public/assets/favicon-16x16.png 	new file:   public/assets/favicon-32x32.png 	new file:   public/assets/favicon.ico 	new file:   public/assets/site.webmanifest

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,8 @@ export default function Home() {
   return (
     <main>
       <Navbar/>
-      <div className="main-hero">
+
+      <section className="main-hero">
         <h1 className="hero-title">Architec<span className="color1">ture</span> </h1>
         <div className="hero-colone1"><div className="blank-hero"></div><Image src={hero1} className="heroimg1" alt="del&co logo"/>
           <div className="hero-col-text">
@@ -29,8 +30,13 @@ export default function Home() {
           </div>
         </div>
         <div className="hero-colone2"><h1>Chez Del&Co, nous proposons des maquettes architecturales innovantes et de haute qualité. Nos maquettes sont conçues pour capturer l'essence de vos projets et donner vie à vos idées de manière tangible et visuellement saisissante. Que vous soyez un architecte, un promoteur immobilier ou un particulier, nos maquettes sont conçues pour répondre à vos besoin.</h1></div>
-        <div className="hero-colone"><Image src={hero2} className="heroimg" alt="del&co logo"/></div>
-      </div>
+        <div className="hero-colone3"><Image src={hero2} className="heroimg" alt="del&co logo"/></div>
+      </section>
+
+      <section className="home-section2" data-aos-duration="700" data-aos="fade-up">
+
+      </section>
+
     </main>
   );
 }
--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,18 +1,55 @@
-import "./navbarCss.css"
+import { useState } from 'react';
+import Image from 'next/image';
 import logo from '@/public/assets/del&co logo.png';
-import Image from "next/image"; 
+import close from '@/public/assets/close.png';
+import "./navbarCss.css";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter } from "@nextui-org/modal";
 
-export default function Navbar(){
-return (
-    <div className="navbar">
-        <div className="navbar-col justify-start"><Image src={logo} className="logo" alt="del&co logo"/></div>
-        <div className="navbar-col justify-end">
-            <a className="nav-link">Home</a>
-            <a className="nav-link">Services</a>
-            <a className="nav-link">A propos</a>
-            <a className="nav-link">Contact</a>
+export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+        setIsModalOpen(!isModalOpen);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setIsOpen(false);
+    };
+
+    return (
+        <div className="navbar">
+            <div className="navbar-col justify-start">
+                <Image src={logo} className="logo" alt="del&co logo" />
+            </div>
+            <div className="navbar-col nav-links">
+                <a href="#" className="nav-link"><p>Home</p></a>
+                <a href="#" className="nav-link"><p>Services</p></a>
+                <a href="#" className="nav-link"><p>A propos</p></a>
+                <a href="#" className="nav-link"><p>Contact</p></a>
+            </div>
+            {!isOpen && (
+                <div className="hamburger" onClick={toggleMenu}>
+                    <div className="hamburger-bar"></div>
+                    <div className="hamburger-bar"></div>
+                    <div className="hamburger-bar"></div>
+                </div>
+            )}
+            {isOpen && (
+                <div className="hamburger-close" onClick={toggleMenu}>
+                    <Image src={close} alt='close' />
+                </div>
+            )}
+            <Modal isOpen={isModalOpen} onClose={closeModal}>
+                <ModalContent>
+                    <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
+                    <ModalBody>
+                        {/* Contenu de votre modal */}
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
         </div>
-
-    </div>
-);
-};
+    );
+}
