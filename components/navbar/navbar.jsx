befec62a7830bdfae7a2c1da7608e1d238bed8be
Delon00 <djplebob32@gmail.com>
2024-05-19 20:12:04 +0000

    modified:   app/globals.css 	modified:   components/navbar/navbar.jsx 	modified:   components/navbar/navbarCss.css

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -7,6 +7,7 @@ import "./navbarCss.css";
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [navbarBg, setNavbarBg] = useState(false);
 
     const openModal = () => {
         setIsModalOpen(true);
@@ -34,8 +35,23 @@ export default function Navbar() {
         }
     }, [isModalOpen]);
 
+    const changeNavbarBg = () => {
+        if (window.scrollY >= 80) {
+            setNavbarBg(true);
+        } else {
+            setNavbarBg(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeNavbarBg);
+        return () => {
+            window.removeEventListener('scroll', changeNavbarBg);
+        };
+    }, []);
+
     return (
-        <div className="navbar">
+        <div className={`navbar ${navbarBg ? 'navbar-bg' : ''}`}>
             <div className="navbar-col justify-start">
                 <Image src={logo} className="logo" alt="del&co logo" />
             </div>
@@ -52,19 +68,25 @@ export default function Navbar() {
                     <div className="hamburger-bar"></div>
                 </div>
             )}
-
-            <div className={`modal ${isModalOpen ? 'active' : ''}`}>
-                <div className="modal-menu">
-                    <div className="modal-title"><h1>Menu</h1> <span onClick={toggleMenu} className='close-modal'><Image src={close} alt='close' /></span></div>
-                    <hr />
-                    <div className='modal-links'>
-                        <a href="#" className="modal-link"><p>Home</p></a>
-                        <a href="#" className="modal-link"><p>Services</p></a>
-                        <a href="#" className="modal-link"><p>A propos</p></a>
-                        <a href="#" className="modal-link"><p>Contact</p></a>
+            {isOpen && (
+                <div className={`modal ${isModalOpen ? 'active' : ''}`}>
+                    <div className="modal-menu">
+                        <div className="modal-title">
+                            <h1>Menu</h1>
+                            <span onClick={toggleMenu} className="close-modal">
+                                <Image src={close} alt="close" />
+                            </span>
+                        </div>
+                        <hr />
+                        <div className="modal-links">
+                            <a href="#" className="modal-link" onClick={closeModal}><p>Home</p></a>
+                            <a href="#services" className="modal-link" onClick={closeModal}><p>Services</p></a>
+                            <a href="#" className="modal-link" onClick={closeModal}><p>A propos</p></a>
+                            <a href="#" className="modal-link" onClick={closeModal}><p>Contact</p></a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 }
